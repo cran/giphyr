2adfb037d628cbe194589c17b525ec27a6c911c5
Hao Zhu <haozhu233@gmail.com>
2018-11-14 21:20:04 +0000

    version 0.1.3

--- R/giphyr-package.R
+++ R/giphyr-package.R
@@ -5,11 +5,11 @@
 #' @importFrom shiny h4 fillRow htmlOutput renderUI textInput actionButton req
 #' HTML observeEvent stopApp dialogViewer runGadget uiOutput actionLink icon
 #' reactiveValues reactive includeCSS includeScript tags img addResourcePath
-#' radioButtons textOutput renderText shinyApp
+#' radioButtons textOutput renderText shinyApp paneViewer
 #' @import miniUI
 #' @importFrom rstudioapi insertText
 #' @importFrom utils download.file
-#' @importFrom purrr map_df
+#' @importFrom purrr map_df map
 #' @importFrom dplyr bind_rows
 #' @name giphyr-package
 #' @aliases giphyr
